531cf53c99abead1575b675d43b9805f6681f9e5
Navy22-anchor <lss4583@gmail.com>
2026-08-10 15:00:56 +0900

    Solve:87-4

--- ch1/2/page79.cpp
+++ ch1/2/page79.cpp
@@ -1,8 +1,8 @@
-//Case 1. include 제외
+//Case 2. <iostream>
 // 이 프로그램은 모니터에 "Hello, World"라는 메시지를 출력한다.
-// #include <iostream>
+#include <ostream>
 // #include <string>
-// using namespace std; 
+using namespace std; 
 
 inline void keep_window_open() {char ch; cin>>ch;}
 
@@ -13,4 +13,4 @@ int main() // C++ 프로그램은 main 함수로 시작한다.
     return 0;
 }
 
-// error: use of undeclared identifier 'cin, 'cout'
+// error: use of undeclared identifier 'cout'; did you mean 'count'? cout을 원했지만 <ostream>에 cout이 없으므로 오류를 냄.
