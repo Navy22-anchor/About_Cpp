cf8c39f699ea80a4ce135cb143b96346816bd2c3
Navy22-anchor <lss4583@gmail.com>
2026-08-10 14:37:41 +0900

    Solve:87-2

---
+++ ch1/2/page79.cpp
@@ -0,0 +1,13 @@
+// 이 프로그램은 모니터에 "Hello, World"라는 메시지를 출력한다.
+#include <iostream>
+#include <string>
+using namespace std; 
+
+inline void keep_window_open() {char ch; cin>>ch;}
+
+int main() // C++ 프로그램은 main 함수로 시작한다.
+{
+    cout << "Hello, World";
+    // keep_window_open();
+    return 0;
+}
