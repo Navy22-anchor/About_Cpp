f267192c2f85f67bd28f5e528222b36598c47d07
Navy22-anchor <lss4583@gmail.com>
2026-08-10 15:02:53 +0900

    Solve:87-4,case3

--- ch1/2/page79.cpp
+++ ch1/2/page79.cpp
@@ -1,6 +1,6 @@
 //Case 2. <iostream>
 // 이 프로그램은 모니터에 "Hello, World"라는 메시지를 출력한다.
-#include <ostream>
+#include <iostream>
 // #include <string>
 using namespace std; 
 
@@ -8,9 +8,9 @@ inline void keep_window_open() {char ch; cin>>ch;}
 
 int main() // C++ 프로그램은 main 함수로 시작한다.
 {
-    cout << "Hello, World";
+    cout << "Hello, World;
     // keep_window_open();
     return 0;
 }
 
-// error: use of undeclared identifier 'cout'; did you mean 'count'? cout을 원했지만 <ostream>에 cout이 없으므로 오류를 냄.
+// error: expected expression
