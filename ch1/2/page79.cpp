8bf70807386665aa6bc1f80c231f9773ef522bee
Navy22-anchor <lss4583@gmail.com>
2026-08-10 15:05:48 +0900

    Solve:87-4,case5

--- ch1/2/page79.cpp
+++ ch1/2/page79.cpp
@@ -1,4 +1,4 @@
-//Case 4. integer
+//Case 4. <
 // 이 프로그램은 모니터에 "Hello, World"라는 메시지를 출력한다.
 #include <iostream>
 // #include <string>
@@ -6,11 +6,11 @@ using namespace std;
 
 inline void keep_window_open() {char ch; cin>>ch;}
 
-integer main() // C++ 프로그램은 main 함수로 시작한다.
+int main() // C++ 프로그램은 main 함수로 시작한다.
 {
-    cout << "Hello, World";
+    cout < "Hello, World";
     // keep_window_open();
     return 0;
 }
 
-// error: unknown type name 'integer'
+// warning: result of comparison against a string literal is unspecified (use an explicit string comparison function instead) [-Wstring-compare]
