ad19dab82cf444581647c18d06282a56d62d5d53
Navy22-anchor <lss4583@gmail.com>
2026-08-10 15:09:40 +0900

    Solve:87-4,caseNormal

--- ch1/2/page79.cpp
+++ ch1/2/page79.cpp
@@ -1,16 +1,12 @@
-//Case 5. ''
 // 이 프로그램은 모니터에 "Hello, World"라는 메시지를 출력한다.
 #include <iostream>
-// #include <string>
 using namespace std; 
 
 inline void keep_window_open() {char ch; cin>>ch;}
 
 int main() // C++ 프로그램은 main 함수로 시작한다.
 {
-    cout << 'Hello, World';
-    // keep_window_open();
+    cout << "Hello, World!\n";
+    keep_window_open();
     return 0;
 }
-
-// warning: multi-character character constant [-Wmultichar]
