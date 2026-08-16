290bb06b512caaf44ed593fe51c184d9134514dd
Navy22-anchor <lss4583@gmail.com>
2026-08-16 22:11:41 +0900

    Solve:4_6

---
+++ ch1/4/typing_alone/practice/ch1_4_6.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    double a = 0;
+    double b = 0;
+    char end {'d'};
+    while (end == '|') {
+        cin >> a;
+        cin >> b;
+        cin >> end;
+        
+        if (a<b) {
+            cout << "더 작은 : " << a << "\t더 큰 : " << b << "\n";
+            if ((b-a)<0.01) {
+            cout << "두수는 거의 같다\n";
+        }
+        }
+        else if (a>b) {
+            cout << "더 작은 : " << b << "\t더 큰 : " << a << "\n";
+            if ((a-b)<0.01) 
+            {
+                cout << "두수는 거의 같다\n";
+            }
+        }
+        else if (a==b) {
+            cout <<  "두수는 같다\n";
+        }
+    }
+}
